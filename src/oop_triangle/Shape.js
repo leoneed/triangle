29aa74104c003727cf9b524269805bec9d872717
Leonid Yeromin <leoneed@leoneed-osx.local>
2016-06-07 03:15:46 +0700

    OOP version. Not ready

---
+++ src/oop_triangle/Shape.js
@@ -0,0 +1,54 @@
+'use strict'
+
+/**
+ * Abstract Constructor to create new Shape
+ *
+ * @constructor
+ */ 
+function Shape() {
+	if (!(this instanceof Shape)) {
+		throw 'Shape must be called as constructor';
+	}
+}
+
+/**
+ * @private shape type
+ */
+Shape.prototype.__type__ = null;
+
+/**
+ * @private shape edges
+ */
+Shape.prototype.__edges__ = [];
+
+/**
+ * Get Shape edges array
+ *
+ * @return {Array}
+ */
+Shape.prototype.getEdges = function() {
+	return this.__edges__;
+}
+
+
+/**
+ * Get type of Shape
+ *
+ * @return {string} current shape type
+ * @return {null} null if shape type is not set
+ */
+Shape.prototype.getType = function() {
+	return this.__type__;
+}
+
+/**
+ * Abstract method to set type of shape
+ *
+ * @param {number} quantity Quantity of equal edges
+ * @return {string} Shape type
+ */
+Shape.prototype.setType = function() {
+	throw 'setType should be implemented in inheritor';
+}
+
+module.exports = Shape;
---
+++ src/oop_triangle/Triangle.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var Shape = require('./Shape.js');
+
+/**
+ * Constructor to create new Shape
+ *
+ * @constructor
+ * @param {Array} edges Edges of shape
+ * @return {Shape} New shape
+ */ 
+function Triangle(edges) {
+	if (!(this instanceof Triangle)) {
+		throw 'Triangle must be called as constructor';
+	}
+
+	this.__edges__ = edges;
+}
+
+Triangle.prototype = new Shape();
+
+/**
+ * @private shape types
+ */
+Triangle.prototype.__types__ = {
+	1: 'scalene',
+	2: 'isosceles',
+	3: 'equilateral'
+};
+
+/**
+ * Implementation of Shape.setType
+ */
+Triangle.prototype.setType = function(quantity) {
+	this.__type__ = this.__types__[quantity];
+}
+
+module.exports = Triangle;
