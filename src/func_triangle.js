eb0438103e6bae5c0cc4c271cecd32a4ec04af7f
Leonid Yeromin <leoneed@leoneed-osx.local>
2016-06-06 23:06:26 +0700

    Project structure and tests

---
+++ src/func_triangle.js
@@ -0,0 +1,12 @@
+/**
+ * Get type of triangle
+ *
+ * @param {number} edge_a
+ * @param {number} edge_b
+ * @param {number} edge_c
+ *
+ * @return {string} possible values: equilateral, isosceles or scalene
+ */
+module.exports = function getTriangleType(edge_a, edge_b, edge_c) {
+
+}
